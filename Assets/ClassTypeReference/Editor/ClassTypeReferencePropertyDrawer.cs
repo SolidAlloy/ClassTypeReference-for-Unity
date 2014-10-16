32d7c2d6d7492f3817a4c0dc9fbc9ae240285605
Lea Hayes <lea@rotorz.com>
2014-10-16 13:34:28 +0000

    Moved `TypeReferences.Editor` namespace into `TypeReferences` for simplicity.

--- Assets/ClassTypeReference/Editor/ClassTypeReferencePropertyDrawer.cs
+++ Assets/ClassTypeReference/Editor/ClassTypeReferencePropertyDrawer.cs
@@ -9,7 +9,7 @@ using System.Reflection;
 using UnityEngine;
 using UnityEditor;
 
-namespace TypeReferences.Editor {
+namespace TypeReferences {
 
 	/// <summary>
 	/// Custom property drawer for <see cref="ClassTypeReference"/> properties.
