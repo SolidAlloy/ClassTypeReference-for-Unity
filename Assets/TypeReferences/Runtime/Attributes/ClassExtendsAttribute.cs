cc05001109e93f03014e8324f0fe0b0f855f2791
SolidAlloy <tadamori.yoshi@gmail.com>
2020-07-11 23:08:14 +0300

    Separated attributes into their own files

---
+++ Assets/TypeReferences/Runtime/Attributes/ClassExtendsAttribute.cs
@@ -0,0 +1,38 @@
+﻿namespace TypeReferences
+{
+    using System;
+
+    /// <summary>
+    /// Constraint that allows selection of classes that extend a specific class when
+    /// selecting a <see cref="ClassTypeReference"/> with the Unity inspector.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field)]
+    public sealed class ClassExtendsAttribute : ClassTypeConstraintAttribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ClassExtendsAttribute"/> class.
+        /// </summary>
+        public ClassExtendsAttribute() { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ClassExtendsAttribute"/> class.
+        /// </summary>
+        /// <param name="baseType">Type of class that selectable classes must derive from.</param>
+        public ClassExtendsAttribute(Type baseType)
+        {
+            BaseType = baseType;
+        }
+
+        /// <summary>
+        /// Gets the type of class that selectable classes must derive from.
+        /// </summary>
+        public Type BaseType { get; private set; }
+
+        /// <inheritdoc/>
+        public override bool IsConstraintSatisfied(Type type)
+        {
+            return base.IsConstraintSatisfied(type)
+                   && BaseType.IsAssignableFrom(type) && type != BaseType;
+        }
+    }
+}
---
+++ Assets/TypeReferences/Runtime/Attributes/ClassTypeConstraintAttribute.cs
@@ -0,0 +1,40 @@
+﻿// Copyright ClassTypeReference Contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root.
+
+namespace TypeReferences
+{
+    using System;
+    using UnityEngine;
+
+    /// <summary>
+    /// Base class for class selection constraints that can be applied when selecting
+    /// a <see cref="ClassTypeReference"/> with the Unity inspector.
+    /// </summary>
+    public class ClassTypeConstraintAttribute : PropertyAttribute
+    {
+        /// <summary>
+        /// Gets or sets grouping of selectable classes. Defaults to <see cref="ClassGrouping.ByNamespaceFlat"/>
+        /// unless explicitly specified.
+        /// </summary>
+        public ClassGrouping Grouping { get; set; } = ClassGrouping.ByNamespaceFlat;
+
+        /// <summary>
+        /// Gets or sets whether abstract classes can be selected from drop-down.
+        /// Defaults to a value of <c>false</c> unless explicitly specified.
+        /// </summary>
+        public bool AllowAbstract { get; set; } = false;
+
+        /// <summary>
+        /// Determines whether the specified <see cref="Type"/> satisfies filter constraint.
+        /// </summary>
+        /// <param name="type">Type to test.</param>
+        /// <returns>
+        /// A <see cref="bool"/> value indicating if the type specified by <paramref name="type"/>
+        /// satisfies this constraint and should thus be selectable.
+        /// </returns>
+        public virtual bool IsConstraintSatisfied(Type type)
+        {
+            return AllowAbstract || !type.IsAbstract;
+        }
+    }
+}
