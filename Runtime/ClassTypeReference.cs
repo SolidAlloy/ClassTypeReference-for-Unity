ee9d353239ee7b8130626499d19e7fb3f2ca75dd
SolidAlloy <tadamori.yoshi@gmail.com>
2020-07-11 22:11:40 +0300

    Changed copyright header in files

--- Runtime/ClassTypeReference.cs
+++ Runtime/ClassTypeReference.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Rotorz Limited. All rights reserved.
+﻿// Copyright ClassTypeReference Contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root.
 
 namespace TypeReferences
