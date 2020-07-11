b7296d880ba3400e3acf318c1fbe29de6d74b0da
SolidAlloy <tadamori.yoshi@gmail.com>
2020-07-11 22:07:58 +0300

    Renamed ClassTypeReference folder into TypeReferences

---
+++ Samples/Demo/ExampleBehaviour.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Rotorz Limited. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root.
+
+namespace Example
+{
+    using System;
+    using TypeReferences;
+    using UnityEngine;
+
+    public class ExampleBehaviour : MonoBehaviour
+    {
+        [ClassImplements(typeof(IGreetingLogger))]
+        public ClassTypeReference GreetingLoggerType = typeof(DefaultGreetingLogger);
+
+        private void Start()
+        {
+            if (GreetingLoggerType.Type == null)
+            {
+                Debug.LogWarning("No greeting logger was specified.");
+            }
+            else
+            {
+                var greetingLogger = Activator.CreateInstance(GreetingLoggerType) as IGreetingLogger;
+                greetingLogger.LogGreeting();
+            }
+        }
+    }
+}
---
+++ Samples/Demo/Special.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Rotorz Limited. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root.
+
+namespace Example
+{
+    using UnityEngine;
+
+    public interface IGreetingLogger
+    {
+        void LogGreeting();
+    }
+
+    public class DefaultGreetingLogger : IGreetingLogger
+    {
+        public void LogGreeting()
+        {
+            Debug.Log("Hello, World!");
+        }
+    }
+
+    public class AnotherGreetingLogger : IGreetingLogger
+    {
+        public void LogGreeting()
+        {
+            Debug.Log("Greetings!");
+        }
+    }
+}
