3e388b91313ac183fe61b328c80f6538ef20003b
SolidAlloy <tadamori.yoshi@gmail.com>
2020-10-04 21:44:41 +0300

    fix: Made TypeReferenceComparer.GetHashCode not accept null Type

--- Runtime/TypeReferenceComparer.cs
+++ Runtime/TypeReferenceComparer.cs
@@ -1,13 +1,17 @@
 ﻿namespace TypeReferences
 {
     using System.Collections.Generic;
+    using UnityEngine.Assertions;
 
     /// <summary>Custom comparer that allows to build TypeReference collections.</summary>
     public class TypeReferenceComparer : IEqualityComparer<TypeReference>
     {
         public bool Equals(TypeReference x, TypeReference y) => x?.Type == y?.Type;
 
-        public int GetHashCode(TypeReference obj) =>
-            string.IsNullOrEmpty(obj.GUID) ? obj.Type.GetHashCode() : obj.GUID.GetHashCode();
+        public int GetHashCode(TypeReference obj)
+        {
+            Assert.IsNotNull(obj.Type);
+            return string.IsNullOrEmpty(obj.GUID) ? obj.Type.GetHashCode() : obj.GUID.GetHashCode();
+        }
     }
 }
