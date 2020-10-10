c674f8a787c6b1e6f5d254beede3594dffa4c022
SolidAlloy <tadamori.yoshi@gmail.com>
2020-10-10 22:13:25 +0300

    feat: Added TypeReference array comparer

--- Runtime/TypeReferenceComparer.cs
+++ Runtime/TypeReferenceComparer.cs
@@ -1,17 +1,42 @@
 ﻿namespace TypeReferences
 {
+    using System.Collections;
     using System.Collections.Generic;
-    using UnityEngine.Assertions;
+    using System.Linq;
 
     /// <summary>Custom comparer that allows to build TypeReference collections.</summary>
-    public class TypeReferenceComparer : IEqualityComparer<TypeReference>
+    public class TypeReferenceComparer : EqualityComparer<TypeReference>
     {
-        public bool Equals(TypeReference x, TypeReference y) => x?.Type == y?.Type;
+        public override bool Equals(TypeReference x, TypeReference y) => x?.Type == y?.Type;
 
-        public int GetHashCode(TypeReference obj)
+        public override int GetHashCode(TypeReference obj)
         {
-            Assert.IsNotNull(obj.Type);
+            if (obj == null || obj.Type == null)
+                return default;
+
             return string.IsNullOrEmpty(obj.GUID) ? obj.Type.GetHashCode() : obj.GUID.GetHashCode();
         }
+    }
+
+    /// <summary>
+    /// Custom comparer for TypeReference arrays to be able to use TypeReference arrays as a key in <see cref="Dictionary{TKey,TValue}"/>.
+    /// </summary>
+    public class TypeReferenceArrayComparer : IEqualityComparer<TypeReference[]>
+    {
+        private readonly TypeReferenceComparer _typeReferenceComparer = new TypeReferenceComparer();
+
+        public bool Equals(TypeReference[] firstArray, TypeReference[] secondArray)
+        {
+            if (firstArray == null && secondArray == null)
+                return true;
+
+            if (firstArray == null || secondArray == null)
+                return false;
+
+            return firstArray.SequenceEqual(secondArray, _typeReferenceComparer);
+        }
+
+        public int GetHashCode(TypeReference[] array) =>
+            ((IStructuralEquatable) array).GetHashCode(_typeReferenceComparer);
     }
 }
