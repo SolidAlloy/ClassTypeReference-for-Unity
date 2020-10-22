ca4624aed9821da3c6936ad88d6721451ae0dfdc
SolidAlloy <tadamori.yoshi@gmail.com>
2020-10-22 22:05:28 +0300

    refactor: Removed unused using statements

--- Runtime/Attributes/TypeOptionsAttribute.cs
+++ Runtime/Attributes/TypeOptionsAttribute.cs
@@ -1,9 +1,7 @@
 ﻿namespace TypeReferences
 {
     using System;
-    using System.Collections.Generic;
     using System.Linq;
-    using System.Reflection;
     using JetBrains.Annotations;
     using SolidUtilities.Extensions;
     using UnityEngine;
